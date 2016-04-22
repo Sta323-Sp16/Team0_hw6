a52edf8a8bd6b6954e0bcaaf55ae606b69ee6c0d
Colin Rundel <rundel@gmail.com>
2016-04-22 13:57:00 -0400

    Spark time example

--- sparkr.R
+++ sparkr.R
@@ -10,7 +10,7 @@ library(magrittr)
 
 ## Starting
 
-sc = sparkR.init()
+sc = sparkR.init("local[4]")
 sqlContext = sparkRSQL.init(sc)
 
 
@@ -49,6 +49,25 @@ res = select(res, explode(res$words))
 res$col = trim(res$col)
 res = group_by(res, "col") %>% count()
 res = arrange(res, desc(res$count))
+
+
+
+## Time Stamps
+
+j = read.json(sqlContext, "hdfs://localhost:8020/data/reddit/large.json")
+
+res = select(j, j$subreddit, j$created_utc) 
+res = mutate(res, created = from_unixtime(res$created_utc)) %>%
+      mutate(., month=month(.$created), wday=date_format(.$created,"E")) 
+res = group_by(res, res$month, res$subreddit) %>% count()
+
+res_jan = filter(res, res$month == 1) %>% arrange(., desc(.$count)) %>% head(n=25)
+res_feb = filter(res, res$month == 2) %>% arrange(., desc(.$count)) %>% head(n=25)
+res_mar = filter(res, res$month == 3) %>% arrange(., desc(.$count)) %>% head(n=25)
+
+save(res_jan,res_feb,res_mar, file="task1.Rdata")
+
+
 
 ## Stopping
 
